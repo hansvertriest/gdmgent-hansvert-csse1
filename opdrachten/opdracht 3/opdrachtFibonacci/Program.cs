0bf97c80e185cb50f09e62d5b681532a0b0de261
hansvertriest <hans.vertriest@gmail.com>
2019-10-15 19:18:03 +0200

    opdracht 3

---
+++ opdrachten/opdracht 3/opdrachtFibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace oef5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengteReeks = Convert.ToInt32(Console.ReadLine());
+		int teller = 0;
+		int oldSom = 0;	/* first number */	
+		int tempSom = 1;	/* second number */
+		Console.WriteLine(oldSom); 
+		Console.WriteLine(tempSom);
+		while (teller < lengteReeks)
+		{
+			int newSom = tempSom + oldSom;
+			oldSom = tempSom;
+			tempSom = newSom;
+			Console.WriteLine(tempSom);
+			teller++;
+		}
+        }
+    }
+}
